f811667e8fe1d007ba6c9c04240c60c7babffa40
Kylie <kszymesko@gmail.com>
2025-03-08 20:26:14 -0500

    Deploy website - based on a5f06b83729509ec39aaa94f3fbee3a282c15fd4

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/kylie/Documents/GitHub/kylie-tests.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kylie/Documents/GitHub/kylie-tests.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kylie/Documents/GitHub/kylie-tests.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/kylie/Documents/GitHub/kylie-tests.github.io/src/css/custom.css"),
-];
